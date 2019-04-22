6905c7497f7f443719dd1f823ecb94010b739d71
mateusbandeiraa <mateusbandeiraa@gmail.com>
2019-04-22 09:48:33 -0300

    Mapa e Player

--- core/src/main/java/entity/GameMap.java
+++ core/src/main/java/entity/GameMap.java
@@ -1,8 +1,12 @@
 package entity;
 
 import java.io.IOException;
+import java.util.PriorityQueue;
 
+import org.mini2Dx.core.game.GameContainer;
 import org.mini2Dx.core.graphics.Graphics;
+import org.mini2Dx.core.screen.GameScreen;
+import org.mini2Dx.core.screen.ScreenManager;
 import org.mini2Dx.tiled.TileLayer;
 import org.mini2Dx.tiled.TiledMap;
 import org.mini2Dx.tiled.collisions.TiledCollisionMapper;
@@ -10,16 +14,56 @@ import org.mini2Dx.tiled.collisions.TiledCollisionMapper;
 import com.badlogic.gdx.files.FileHandle;
 
 public class GameMap extends TiledMap {
-	
-	private float positionX = 0;
-	private float positionY = 0;
-	
+	private int positionX = 0;
+	private int positionY = 0;
+	private float renderScale = 1f;
+	private float mapOffsetScale = 1.5f;
+	private PriorityQueue<GameObject> objects;
+
 	private byte[][] collisions;
-	
+
 	public GameMap(FileHandle fileHandle) throws IOException {
 		super(fileHandle);
+		STRICT_LAYER_VISIBILITY = true; // True para não renderizar layers ocultas, como a Collisions.
 		collisions = TiledCollisionMapper.mapCollisionsByLayer(this, "Collisions");
-		
+		objects = new PriorityQueue<GameObject>();
+
+	}
+
+	public void update(GameContainer gc, ScreenManager<? extends GameScreen> screenManager, float delta) {
+		super.update(delta);
+
+		int tiledMapWidth = this.getPixelWidth();
+		int tiledMapHeight = this.getPixelHeight();
+		float gcWidth = gc.getWidth();
+		float gcHeight = gc.getHeight();
+
+		renderScale = ((gcWidth - tiledMapWidth) < (gcHeight - tiledMapHeight) ? gcWidth / tiledMapWidth
+				: gcHeight / tiledMapHeight) * mapOffsetScale;
+
+		float scaledTiledMapWidth = tiledMapWidth * renderScale;
+		float scaledTiledMapHeight = tiledMapHeight * renderScale;
+
+		positionX = (int) ((gcWidth - scaledTiledMapWidth) / (2 * renderScale));
+		positionY = (int) ((gcHeight - scaledTiledMapHeight) / (2 * renderScale));
+
+		objects.forEach(gameObject -> gameObject.update(gc, delta));
+	}
+
+	public void render(GameContainer gc, Graphics g) {
+		g.scale(renderScale, renderScale);
+		this.draw(g, positionX, positionY);
+
+		objects.forEach(gameObject -> gameObject.render(gc, g));
+		g.scale(1, 1);
+	}
+
+	public void addObject(GameObject o) {
+		objects.add(o);
+	}
+
+	public void removeObject(GameObject o) {
+		objects.remove(o);
 	}
 
 	@Override
@@ -27,23 +71,16 @@ public class GameMap extends TiledMap {
 			int heightInTiles) {
 		super.onLayerRendered(g, layer, startTileX, startTileY, widthInTiles, heightInTiles);
 	}
-	
-	@Override
-	public void draw(Graphics g, int x, int y) {
-		this.positionX = x;
-		this.positionY = y;
-		super.draw(g, x, y);
-	}
-	
-	public byte[][] getCollisions(){
+
+	public byte[][] getCollisions() {
 		return collisions;
 	}
-	
+
 	// FIXME: Não acho que esse é um bom nome.
 	public float getPositionXbyTile(int tile) {
 		return positionX + (tile * this.getTileWidth());
 	}
-	
+
 	public float getPositionYbyTile(int tile) {
 		return positionY + (tile * this.getTileHeight());
 	}
--- core/src/main/java/screen/IndoorScreen.java
+++ core/src/main/java/screen/IndoorScreen.java
@@ -27,11 +27,12 @@ public class IndoorScreen extends BasicGameScreen {
 			ex.printStackTrace();
 		}
 		player = new Player(16, 18, tiledMap);
+		 tiledMap.addObject(player);
 	}
 
 	@Override
 	public void update(GameContainer gc, ScreenManager<? extends GameScreen> screenManager, float delta) {
-		player.update(gc, delta);
+		tiledMap.update(gc, screenManager, delta);
 	}
 
 	@Override
@@ -42,19 +43,7 @@ public class IndoorScreen extends BasicGameScreen {
 
 	@Override
 	public void render(GameContainer gc, Graphics g) {
-		// FIXME: Essa lógica não deveria estar no método render.
-		float scale = 1.5f;
-		g.setScale(scale, scale);
-		float tiledMapWidth = tiledMap.getPixelWidth() * scale;
-		float tiledMapHeight = tiledMap.getPixelHeight() * scale;
-		float gcWidth = gc.getWidth();
-		float gcHeight = gc.getHeight();
-		float posX = (gcWidth - tiledMapWidth) / (2 * scale);
-		float posY = (gcHeight - tiledMapHeight) / (2 * scale);
-
-		tiledMap.draw(g, (int) posX, (int) posY);
-
-		player.render(gc, g);
+		tiledMap.render(gc, g);
 	}
 
 	@Override
