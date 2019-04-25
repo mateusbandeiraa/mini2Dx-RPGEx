d3117cdb34dd7e6c591f71b7bc481105deb165f0
mateusbandeiraa <mateusbandeiraa@gmail.com>
2019-04-24 23:00:35 -0300

    JavaDoc Player

--- core/src/main/java/entity/Player.java
+++ core/src/main/java/entity/Player.java
@@ -2,15 +2,37 @@ package entity;
 
 import org.mini2Dx.core.game.GameContainer;
 import org.mini2Dx.core.graphics.Graphics;
+import org.mini2Dx.tiled.Tile;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
 
+/**
+ * Representa o jogador na tela.
+ * 
+ * Criado em 24/04/19
+ * 
+ * @author Mateus Bandeira
+ */
 public class Player extends GridObject {
-	
+	/**
+	 * Vida atual deste objeto.
+	 */
 	private Health health;
 
+	/**
+	 * Constrói uma instância deste objeto.
+	 * 
+	 * @param x
+	 *            Coordenada X deste objeto em unidades de {@link Tile}, relativo ao
+	 *            mapa.
+	 * @param y
+	 *            Coordenada Y deste objeto em unidades de {@link Tile}, relativo ao
+	 *            mapa.
+	 * @param map
+	 *            Mapa ao qual este objeto está vinculado.
+	 */
 	public Player(int x, int y, GameMap map) {
 		super(x, y, new Texture("tanks_tankDesert2.png"), map);
 		health = new Health(10, 10, this);
@@ -23,12 +45,12 @@ public class Player extends GridObject {
 		int candidateY = this.gridY;
 
 		if (Gdx.input.isKeyJustPressed(Keys.RIGHT)) {
-			if(sprite.isFlipX()) {
+			if (sprite.isFlipX()) {
 				sprite.flip(true, false);
 			}
 			candidateX++;
 		} else if (Gdx.input.isKeyJustPressed(Keys.LEFT)) {
-			if(!sprite.isFlipX()) {
+			if (!sprite.isFlipX()) {
 				sprite.flip(true, false);
 			}
 			candidateX--;
@@ -42,10 +64,10 @@ public class Player extends GridObject {
 			this.gridX = candidateX;
 			this.gridY = candidateY;
 		}
-		
+
 		health.update(gc, delta);
 	}
-	
+
 	@Override
 	public void render(GameContainer gc, Graphics g) {
 		super.render(gc, g);
